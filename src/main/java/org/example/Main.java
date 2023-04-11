0d7881a931cba61622f2ff31a6572d542f4497dd
Serge Taras <starasqa@gmail.com>
2023-04-10 20:43:48 -0400

    Add pom file plus changes in main - 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("My new file");
+        System.out.println("My new file.");
+        System.out.println("One more option.");
     }
 }
